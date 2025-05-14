417cd9f8d0831bfefc1321e5b0e0f6a2f212cc07
Jouini Med youssef <168489700+MohamedYoussefjo@users.noreply.github.com>
2025-05-14 12:46:12 +0200

    Update preprocessproduce.py

--- mypy/preprocessproduce.py
+++ mypy/preprocessproduce.py
@@ -93,7 +93,6 @@ def main():
             .config("spark.sql.files.maxPartitionBytes", "128MB") \
             .config("spark.sql.streaming.schemaInference", "true")\
             .config("spark.sql.shuffle.partitions", "4")\
-            .config("spark.executor.memory", "1g")  \
             .config("spark.task.maxFailures", "4")\
             .getOrCreate()
             
